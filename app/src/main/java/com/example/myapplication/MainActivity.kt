26c9fe90d1e89a07e15a9f91166d66c11c3de068
Streubert <knarom14+github1@gmail.com>
2019-12-23 15:50:25 +0100

    Added button which increments the number shown by the TextView on click.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,14 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var number = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tv_test.text = "Das ist ein Test!"
+        tv_test.text = number.toString()
+        btn_test.text = "Click me!"
+
+        btn_test.setOnClickListener(View.OnClickListener {
+            number++
+            tv_test.text = number.toString()
+        })
     }
 }
